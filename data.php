35cd2d3edc6fafa4527c6e1c9fccae693b3ec40f
daisuke1591 <81580679+daisuke1591@users.noreply.github.com>
2021-03-30 02:52:57 +0900

    Create data.php

---
+++ data.php
@@ -0,0 +1,47 @@
+<?php require_once('class.php') ;
+
+
+$que1 = new Question("【1】コンタクトはされていますか？");
+$que2 = new Question("【2】お肌の状態・肌質");
+$que3 = new Question("【3】FACEの悩み");
+$que4 = new Question("【4】日頃お使いの化粧品とお手入れ方法");
+$que4m = new Question("朝");
+$que4n = new Question("夜");
+$que5 = new Question("【5】アレルギー");
+$que6 = new Question("【6】毎日の生活リズム");
+$que6a = new Question("食事");
+$que6b = new Question("アルコール");
+$que6c = new Question("タバコ");
+$que6d = new Question("コーヒー");
+$que6e = new Question("補助食品");
+$que6f = new Question("くすり");
+$que6g = new Question("睡眠");
+$que6h = new Question("運動");
+$que6i = new Question("仕事");
+$que6j = new Question("ストレス");
+$que7 = new Question("【7】BODYの悩み");
+$que8 = new Question("【8】Healthの悩み");
+$que9 = new Question("【9】興味があるもの");
+$que10 = new Question("送信");
+
+$ques = array($que1,$que2,$que3,$que4,$que4m,$que4n,$que5,$que6,$que6a,$que6b,$que6c,$que6d,$que6e,$que6f,$que6g,$que6h,$que6i,$que6j,$que7,$que8,$que9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
